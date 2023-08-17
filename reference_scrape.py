ee6583f027cc66681c89a65695346c504dafd5cc
Cole Coughlin <ccoughlin@perimeterinstitute.ca>
2023-08-17 15:54:25 -0400

    first attempt at reference scraper

--- reference_scrape.py
+++ reference_scrape.py
@@ -1,13 +1,33 @@
 # your_script.py
 import sys
+import re
+
+def extract_citations(latex_text):
+    citations = re.findall(r'\\cite{([^}]*)}', latex_text)
+    cited_references = [reference.strip() for citation in citations for reference in citation.split(',')]
+    return cited_references
+
+def count_references(cited_references):
+    reference_count = {}
+    for reference in cited_references:
+        if reference in reference_count:
+            reference_count[reference] += 1
+        else:
+            reference_count[reference] = 1
+    return reference_count
 
 def process_file(file_path):
-    # Your file processing logic here
-    with open(file_path, 'r') as file:
-        content = file.read()
-        processed_data = content.upper()  # Example: Convert text to uppercase
+    with open(file_path, 'r', encoding='utf-8') as file:
+        latex_text = file.read()
 
-    return processed_data
+    cited_references = extract_citations(latex_text)
+    reference_count = count_references(cited_references)
+
+    print("List of cited references:")
+    for reference, count in reference_count.items():
+        print(f"{reference}: {count} times")
+    
+    return cited_references
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -17,3 +37,9 @@ if __name__ == '__main__':
     input_file_path = sys.argv[1]
     result = process_file(input_file_path)
     print(result)
+
+
+
+
+
+
